9142f2f65cdf1b34fb41e8ed0bf34af5be5e5c9a
Prakhar Sahu <prakhars762@gmail.com>
2025-10-18 19:50:11 +0530

    bug fix

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -97,18 +97,19 @@ const ModelScroll = () => {
 
 const Features = () => {
   return (
-    <section className="relative h-screen">
+    <section id="features">
+      <h2>See it all in a new light.</h2>
+
       <Canvas id="f-canvas" camera={{}}>
         <StudioLights />
         <ambientLight intensity={0.5} />
-        {/* 3D MODEL*/}
         <ModelScroll />
       </Canvas>
 
       <div className="absolute inset-0">
         {features.map((feature, index) => (
           <div
-            key={index}
+            key={feature.id}
             className={clsx("box", `box${index + 1}`, feature.styles)}
           >
             <img src={feature.icon} alt={feature.highlight} />
